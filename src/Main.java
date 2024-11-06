f2523d2e9f56f5b14d275f860842f82263642e1f
momadAB <mabaqer99@gmail.com>
2024-11-06 09:30:29 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-
-    Person teacher = new Teacher("Mista White", "J.P. Wynne High School");
+    Person teacher = new Teacher("Mista White", "J.P. Wynne High School", "Chemistry");
     teacher.describeRole();
   }
 }
--- src/Teacher.java
+++ src/Teacher.java
@@ -1,13 +1,15 @@
 public class Teacher extends Person {
   private final String school;
+  private final String subject;
 
-  public Teacher(String name, String school) {
+  public Teacher(String name, String school, String subject) {
     super(name);
     this.school = school;
+    this.subject = subject;
   }
 
   @Override
   public void describeRole() {
-    System.out.printf("%s teaches at %s.%n", getName(), school);
+    System.out.printf("%s teaches %s at %s.%n", getName(), subject, school);
   }
 }
